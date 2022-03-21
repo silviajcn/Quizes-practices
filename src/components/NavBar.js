3083f571269bfae911148e52573af8e189be8b9d
Silvia <silviajcn@gmail.com>
2022-03-20 21:35:06 -0500

    change navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import '../styles/elements.css';
 
 const NavBar = () => {
     return (
-        <header>
+        <header className='header-navbar'>
             <Link to="/">
                 <img src="https://res.cloudinary.com/silviajcn/image/upload/v1645900258/PRACTICAS/Quizes/logo/logo_smk8tr.png" alt="logo" className='logo' />
-                <p>Quiz App</p>
+                <p className='links name-app'>Quiz App</p>
             </Link>
         </header>
     )
